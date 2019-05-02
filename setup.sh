9606902d6fb57fc3b6cc5f97674a04a2a195039b
trig0n <pascal@eberlein.io>
2019-05-02 23:47:28 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-protoc --cpp_out=vector_tile_spec/out/ vector_tile_spec/2.1/vector_tile.proto
-mv vector_tile_spec/out/vector_tile_spec/2.1/vector_tile.pb.cc libs/vector_tile21/vector_tile.pb.cc
-mv vector_tile_spec/out/vector_tile_spec/2.1/vector_tile.pb.h libs/vector_tile21/vector_tile.pb.h
+mkdir vector_tile_spec
+cd vector_tile_spec
+if [ ! -f "vector_tile.proto" ]; then
+    wget https://raw.githubusercontent.com/mapbox/vector-tile-spec/master/2.1/vector_tile.proto
+fi
+sed '3d' vector_tile.proto > vt.proto
+rm vector_tile.proto
+mv vt.proto vector_tile.proto
+if [ ! -d "out" ]; then
+    mkdir out
+fi
+if [ ! -f "out/vector_tile.pb.cc" ]; then
+    protoc --cpp_out=out/ vector_tile.proto
+    mv out/vector_tile.pb.cc ../libs/vector_tile21/vector_tile.pb.cc
+    mv out/vector_tile.pb.h ../libs/vector_tile21/vector_tile.pb.h
+fi
